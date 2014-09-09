dd35bfedcefc133ee47a4d5d9f7686427b490f52
stevenio <steven.yhw@gmail.com>
2014-09-09 10:12:27 -0500

    init dcorr.py full.pdb na.pdb (no actual code yet)

---
+++ python/2.7/dcorr.py
@@ -0,0 +1,16 @@
+"""
+Goal: calculate the cross correlation of inter-residue distances
+Author: Steven(Yuhang) Wang
+Date: Sep-09-2014
+
+Blueprint:
+1. extract all-atom coordinate set using [[ProDy]] module
+2. calculate the inter-residue distance matrix, and save as data frames using [[Pandas]] module
+
+Dependencies:
+Name     |     Link
+-------- | -------------
+Prody    | http://prody.csb.pitt.edu/downloads
+Pandas   | http://pandas.pydata.org
+-------- |  
+"""
