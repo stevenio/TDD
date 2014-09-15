34e9cd9a28a431bd8161b45f097908d206b39d63
stevenio <steven.yhw@gmail.com>
2014-09-15 10:55:04 -0500

    add a dcd file into data/

--- python/2.7/dcorr.py
+++ python/2.7/dcorr.py
@@ -7,10 +7,16 @@ Blueprint:
 1. extract all-atom coordinate set using [[ProDy]] module
 2. calculate the inter-residue distance matrix, and save as data frames using [[Pandas]] module
 
-Dependencies:
+External Dependencies:
 Name     |     Link
 -------- | -------------
 Prody    | http://prody.csb.pitt.edu/downloads
 Pandas   | http://pandas.pydata.org
 -------- |  
 """
+
+#--------------------#
+# Python Builtins    #
+#--------------------#
+import unittest 
+
