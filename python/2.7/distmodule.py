2bb8a8fdccc38deaa31603a71249007f42ea874c
stevenio <steven.yhw@gmail.com>
2014-09-16 11:55:58 -0500

    added cenmass function to distmodule.py

--- python/2.7/distmodule.py
+++ python/2.7/distmodule.py
@@ -4,4 +4,34 @@ Goal: provide a module of small functions for any
 Author: Yuhang Wang
 Date: 2014-09-16
 """
+#============================================================
+# Compatibility with Python 3
+#============================================================
+from __future__ import print_function, division
+#============================================================
 
+#============================================================
+# Dependencies
+#============================================================
+import scipy
+#import scipy.linalg
+#============================================================
+
+def centmass(coords_matrix,mass_vector):
+  """
+  Return the center of mass coordinates for
+    a set of coordinates.
+  Inputs: coords_matrix << Nx3 matrix(numpy.array object) of x,y,z coordinates
+          mass_vector   << Nx1 vector(numpy.array object) of masses 
+  """
+  totalMass = scipy.sum(mass_vector)
+  return 1.0/totalMass*scipy.dot(coords_matrix.T, mass_vector)
+
+
+if __name__ == "__main__":
+  print("main")
+  M = scipy.arange(0,9, dtype=scipy.float64).reshape(3,3)
+  b = scipy.array([1,1,1], dtype=scipy.float64)
+  print("M=\n",M)
+  print("b=\n",b)
+  print(centmass(M,b))
