31e8d1e24f9399b5f35e87af866721b6c6df222e
stevenio <steven.yhw@gmail.com>
2014-09-19 17:42:42 -0500

    feature get_commute_time done!

--- python/2.7/Test_allostery.py
+++ python/2.7/Test_allostery.py
@@ -24,6 +24,7 @@ import allostery
 #============================================================
 
 class TestAllostery(unittest.TestCase):
+  
   def setUp(self):
     print("-----Setting up {0} -------".format(self.__class__.__name__))
     #------------------------------------------------------------
@@ -35,7 +36,7 @@ class TestAllostery(unittest.TestCase):
       trajectory_name = "sod.pdb"
       psf_name = "sod.psf"
     elif case == 2:
-      trajectory_name = "segA3.pdb"
+      trajectory_name = "segA3.dcd"
       psf_name = "segA3.psf"
     elif case == 3:
       trajectory_name = "segA.pdb"
@@ -45,27 +46,47 @@ class TestAllostery(unittest.TestCase):
     self.my_trajectory = os.path.join(data_dir, trajectory_name)
     self.my_psf = os.path.join(data_dir, psf_name)
     self.allos = allostery.Allostery(data_dir, self.my_psf, self.my_trajectory)
+    #-------------------
+    # for testing
+    #------------------------
+    self.resid_list = range(41,43)
+    self.segid_list = ['A']
+    self.extra_criteria = "not name H*"
 
   def tearDown(self):
     print("-----Tearing down {0} -------".format(self.__class__.__name__))
     del self.allos
 
   def test_instantiation(self):
-    assert self.allos.psf_filename == self.my_psf
-    assert self.allos.trajectory_filename == self.my_trajectory
+    self.assertEqual(self.allos.psf_filename, self.my_psf)
+    self.assertEqual(self.allos.trajectory_filename, self.my_trajectory)
 
-  def test_set_resid_list(self):
-    resid_list = range(1,10)
-    self.allos.set_resid_list(resid_list)
-    self.assertEqual(self.allos.resid_list, resid_list)
+  def test_set_basal_selection(self):
+    self.allos.set_basal_selection(self.segid_list, self.extra_criteria)
+    self.assertEqual(self.allos.segid_list, self.segid_list)
+    selection_str = "(segid {0}) and ({1})".format(self.segid_list[0], self.extra_criteria)
+    self.assertEqual(self.allos.basal_selection_str, selection_str)
 
-  def test_set_pair_dist_matrix(self):
-    resid_list = range(1,10)
-    N = len(resid_list)
-    self.allos.set_resid_list(resid_list)
-    self.allos.set_pair_dist_matrix()
-    self.assertEqual(numpy.shape(self.allos.pair_dist_matrix), (N,3))
+  def test_build_com_coords(self):
+    N = len(self.resid_list)
+    self.allos.set_basal_selection(self.segid_list, self.extra_criteria)
+    self.allos.build_com_coords(self.resid_list, self.segid_list, self.extra_criteria)
+    self.assertEqual(numpy.shape(self.allos.com_coords), (N,3))
+  @unittest.skip("")
+  def test_build_pair_dist_matrix(self):
+    resid_list = range(41,43)
+    self.allos.build_com_pair_dist_matrix(resid_list, self.segid_list, self.extra_criteria)
+    result = numpy.array([[ 0.        , 4.59339682],
+                          [ 4.59339682,  0.        ]], dtype=numpy.float32)
+    tol = 1E-5;
+    self.assertTrue((numpy.abs(self.allos.com_pair_dist_matrix - result) < tol).all())
+  
+  def test_get_commute_time(self):
+    self.allos.get_commute_time(self.resid_list, self.segid_list, self.extra_criteria)
+    print("commute time\n", self.allos.commute_time)
+    
+    
 
-
+    
 if __name__ == '__main__':
   unittest.main()
--- python/2.7/allostery.py
+++ python/2.7/allostery.py
@@ -1,7 +1,7 @@
 """
 Goal: compute residue pairwise center of mass distances
 Author: Yuhang Wang
-Date: 09-15-2014
+Date: 09-17-2014 
 """
 #============================================================
 # Compatibility with python 3
@@ -33,23 +33,99 @@ class Allostery(object):
     self.trajectory_filename = os.path.join(data_src_dir, trajectory_filename)
     self.universe = mda.Universe(self.psf_filename, self.trajectory_filename)
     self.resid_list   = []
-    self.pair_dist_matrix = False
+    self.segid_list = False
+    self.com_coords = False
+    
 
-  def set_resid_list(self, resid_list):
+  def set_selection_str(self, keyword, member_list):
     """
-    Set up list of residue IDs, to be used for residual pairwise distance
+    return of string like this "(segid 1 or segid 2)",
+    where keyword is "segid" and member_list is [1,2]
+    """
+    S = []
+    for member in member_list:
+      S.append("{0} {1}".format(keyword, member))
+    return "({0})".format(" or ".join(S))
+    
+  def set_basal_selection(self, segid_list, extra_criteria="all"):
+    """
+    Set up base selection string using segid's, [and extra_criteria], 
+    to be used for residue pairwise distance
     calculation.
     """
-    self.resid_list = resid_list
 
-  def set_pair_dist_matrix(self): 
+    self.segid_list = segid_list
+    self.extra_criteria = extra_criteria
+    S  = []
+    S.append(self.set_selection_str("segid", segid_list))
+    self.basal_selection_str = " and ".join(S)
+    self.basal_selection_str += " and ({0})".format(extra_criteria)
+    
+
+  
+  def build_com_coords(self, resid_list, segid_list, extra_criteria="all"): 
     """
-    Initialize self.pair_dist_matrix (to be filled later)
+    build an Nx3 matrix of center of mass coordinates for selected residues in resid_list
     """
+    self.set_basal_selection(segid_list, extra_criteria)
+    self.resid_list = resid_list
     N = len(self.resid_list)
-    self.pair_dist_matrix = numpy.zeros((N,3))
+    self.com_coords = numpy.zeros((N,3), dtype=numpy.float32)
+    ccc = 0 # counter
+    for resid in self.resid_list:
+      selection_str = self.basal_selection_str + " and resnum {0}".format(resid)
+      tmp_selection = self.universe.selectAtoms(selection_str)
+      #------------------------------------------------
+      # in case zero atoms got selected
+      #------------------------------------------------
+      if tmp_selection.numberOfAtoms() == 0:
+        msg = "atom selection [{0}] yields zero atoms".format(selection_str)
+        raise UserWarning(msg)
+      #--------------------------------------------------
+      X = tmp_selection.get_positions()
+      M = tmp_selection.masses()
+      self.com_coords[ccc,:] = tmp_selection.centerOfMass(pbc=True)
+      ccc += 1
+
+  def build_com_pair_dist_matrix(self, resid_list, segid_list, extra_criteria="all"):
+    """
+    build pair-wise distance matrix for center of mass coordinates
+    """
+    self.build_com_coords(resid_list, segid_list, extra_criteria)
+    #-----------------
+    # CALL MDAnalysis.core.distances.distance_array
+    # instead of the MDAnalysis.core.parallel.distances (which requires inputs to be of Cython DTYPE_t type)
+    self.com_pair_dist_matrix = mda.core.distances.distance_array(self.com_coords, self.com_coords)
     
+  def get_commute_time(self, resid_list, segid_list, extra_criteria="all"):
+    """
+    build commute time matrix based on pair-wise com distance matrix
+    """
+
+    ccc = 0 # frame counter
+    for fr in self.universe.trajectory:
+      frameId = fr.frame
+      ccc += 1
+      print("frame: {0}".format(fr.frame))
+      #---------------------------------------------------------
+      # build self.com_pair_dist_matrix for the current matrix
+      #----------------------------------------------
+      self.build_com_pair_dist_matrix(resid_list, segid_list, extra_criteria)
+      #------------------
+      #  initialize 
+      #-----------------------
+      if frameId == 1:
+        self.commute_time = numpy.zeros(numpy.shape(self.com_pair_dist_matrix), dtype=numpy.float32)
+      #-----------------------------------
+      self.commute_time += self.com_pair_dist_matrix
+    #----------------------------
+    # compute the average
+    #-----------------------------------
+    self.commute_time /= ccc
     
+
+
+
 
 def test():
   #------------------------------------------------------------
@@ -83,7 +159,7 @@ def test():
     print("\nframe: {0}\n".format(fr.frame))
     print(numpy.shape(SL.get_positions()))
     print((SL.get_positions()))
-    X = SL.get_positions
+    X = SL.get_positions()
     massVector = SL.masses()
     pairDist = mda_dist.distance_array(SL.get_positions(),SL.get_positions())
     print(pairDist.shape)
