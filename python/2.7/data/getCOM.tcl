4e6db9c52dd4aa51b005d39f0a2e4a693a94f890
stevenio <steven.yhw@gmail.com>
2014-09-30 17:17:54 -0500

    add getCOM.tcl and segA3_com.dat

---
+++ python/2.7/data/getCOM.tcl
@@ -0,0 +1,28 @@
+# calculate center of mass distance between residues
+# in order to generate  comparison data set for debugging
+# the python code
+
+set base_filename "segA3"
+set psf_filename ${base_filename}.psf
+set trajectory_filename ${base_filename}.pdb
+set output_filename "${base_filename}_com.dat"
+
+
+set OUT [open $output_filename w]
+
+
+set molId [mol new $psf_filename ]
+mol addfile $trajectory_filename watifor all
+
+set S [atomselect top "name CA"]
+set resids [$S get resid]
+
+set extra "noh"
+foreach resid_1 $resids {
+      set S1 [atomselect top "resid $resid_1 and $extra"]
+      set com1 [measure center $S1 weight mass]
+      puts $OUT "$com1"
+}
+
+close $OUT
+exit
